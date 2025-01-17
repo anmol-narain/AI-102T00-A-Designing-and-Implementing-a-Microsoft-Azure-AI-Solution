7f72c7862303c3116ddd55d8fa403f17dbf823b0
anmol-narain <anmolnarain83@gmail.com>
2025-01-17 03:39:57 -0800

    Initial commit with changes

--- Labfiles/06-use-own-data/Python/ownData.py
+++ Labfiles/06-use-own-data/Python/ownData.py
@@ -30,6 +30,16 @@ def main():
         text = input('\nEnter a question:\n')
 
         # Configure your data source
+        extension_config = dict(dataSources = [  
+                { 
+                    "type": "AzureCognitiveSearch", 
+                    "parameters": { 
+                        "endpoint":azure_search_endpoint, 
+                        "key": azure_search_key, 
+                        "indexName": azure_search_index,
+                    }
+                }]
+            )
 
 
         # Send request to Azure OpenAI model
@@ -64,7 +74,7 @@ def main():
         print(ex)
 
 
-if __name__ == '__main__': 
+if __name__ == '__init__()': 
     main()
 
 
